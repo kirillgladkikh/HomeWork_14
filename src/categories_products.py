d464d78cf7c1e67e5ff4007c6936707395e34033
kirill.gladkikh <k.gladkikh@mail.ru>
2025-10-05 22:11:16 +0300

    HW 14.3 magic methods done

--- src/categories_products.py
+++ src/categories_products.py
@@ -36,6 +36,24 @@ class Product:
             f"В наличии: {self.quantity} шт."
         )
 
+    # Для класса Product добавить строковое отображение в следующем виде: Название продукта, 80 руб. Остаток: 15 шт.
+    def __str__(self):
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт."
+
+    # Для класса Product переопределен магический метод сложения __add__, который принимает два аргумента:
+    # self и второй объект.
+    # Метод возвращает сумму произведений цены на количество у двух объектов.
+    #
+    # Для удобства работы с продуктами реализовать возможность их складывать.
+    # Логика сложения должна работать так, чтобы в итоге у вас получалась полная стоимость всех товаров на складе.
+    # Например, для товара А с ценой 100 рублей и количеством на складе 10 и товара B с ценой 200 рублей
+    # и количеством на складе 2
+    # результатом выполнения операции А + B должно стать значение, полученное из 100 × 10 + 200 × 2 = 1400.
+    def __add__(self, other):
+        if isinstance(other, Product):
+            return self.price * self.quantity + other.price * other.quantity
+        raise TypeError("Сложение возможно только между объектами класса Product")
+
 
 class Category:
     total_categories = 0
@@ -56,11 +74,19 @@ class Category:
         else:
             raise TypeError("Можно добавлять только объекты класса Product")
 
+    # Tеперь каждый продукт имеет реализованное строковое отображение.
+    # Геттер оптимизирован, преобразовав объект продукта в строку.
     @property
     def products(self) -> str:
-        return "\n".join(
-            [f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n" for product in self.__products]
-        )
+        return "\n".join(str(product) for product in self.__products)
 
-    def __str__(self) -> str:
-        return f"Категория: {self.name}\n" f"Описание: {self.description}\n" f"Товары в категории:\n{self.products}"
+    # def __str__(self) -> str:
+    #     return f"Категория: {self.name}\n" f"Описание: {self.description}\n" f"Товары в категории:\n{self.products}"
+
+    # Добавляет метод __str__ в класс Category, который возвращает строку в указанном формате:
+    # Название категории, количество продуктов: 200 шт.
+    def __str__(self):
+        # Подсчитываем общее количество товаров во всех продуктах категории
+        total_quantity = sum(product.quantity for product in self.__products)
+        # Формируем строку в требуемом формате
+        return f"Категория: {self.name}, количество продуктов: {total_quantity} шт."
